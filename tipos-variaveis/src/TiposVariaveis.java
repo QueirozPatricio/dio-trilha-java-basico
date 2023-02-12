c271e11808315dcec0ddd224fdd32d5e8650a99a
Patricio Queiroz <patricio_queiroz@yahoo.com.br>
2023-02-12 11:11:46 -0300

    Atualizada com alguns tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;//adicionou um cast
+
+        //variáveis e constantes
+        //Variável
+        int numero = 5;
+        numero = 10;
+
+        //Constante
+        final double VALOR_DE_PI = 3.14;//Não pode ser alterada
+
+        System.out.println(numero);
+
+
+
     }
 }
